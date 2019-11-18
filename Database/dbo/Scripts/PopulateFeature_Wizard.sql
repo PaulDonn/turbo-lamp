c80ab9976317798b9e181a0eb5f4f441e93f8040
Paul Donnelly <paul.donnelly.91@gmail.com>
2019-11-18 00:39:38 +0000

    Update PopulateFeature_Wizard.sql

--- Database/dbo/Scripts/PopulateFeature_Wizard.sql
+++ Database/dbo/Scripts/PopulateFeature_Wizard.sql
@@ -9,24 +9,24 @@
 		   ),
            ('Your Spellbook'--<Name, nvarchar(50),>
            ,'The Spells that you add to your Spellbook as you gain levels reflect the arcane Research you conduct on your own, as well as intellectual breakthroughs you have had about the Nature of the multiverse. You might find other Spells during your Adventures. You could discover a spell recorded on a scroll in an evil wizard’s chest, for example, or in a dusty tome in an ancient Library.
-
-Copying a Spell into the Book: When you find a Wizard spell of 1st level or higher, you can add it to your Spellbook if it is of a Spell Level you can prepare and if you can spare the time to decipher and copy it.
-
-Copying that spell into your Spellbook involves reproducing the basic form of the spell, then deciphering the unique system of notation used by the Wizard who wrote it. You must practice the spell until you understand the sounds or gestures required, then transcribe it into your Spellbook using your own notation.
-
-For each level of the spell, the process takes 2 hours and costs 50 gp. The cost represents material Components you expend as you experiment with the spell to Master it, as well as the fine inks you need to record it. Once you have spent this time and money, you can prepare the spell just like your other Spells.
-
-Replacing the Book: You can copy a spell from your own Spellbook into another book—for example, if you want to make a backup copy of your Spellbook. This is just like copying a new spell into your Spellbook, but faster and easier, since you understand your own notation and already know how to cast the spell. You need spend only 1 hour and 10 gp for each level of the copied spell.
-
-If you lose your Spellbook, you can use the same procedure to transcribe the Spells that you have prepared into a new Spellbook. Filling out the remainder of your Spellbook requires you to find new Spells to do so, as normal. For this reason, many wizards keep backup spellbooks in a safe place.
-
-The Book’s Appearance: Your Spellbook is a unique compilation of Spells, with its own decorative flourishes and margin notes. It might be a plain, functional leather volume that you received as a gift from your Master, a finely bound gilt-­edged tome you found in an ancient Library, or even a loose collection of notes scrounged together after you lost your previous Spellbook in a mishap.'--<Description, nvarchar(max),>
+		   
+		   Copying a Spell into the Book: When you find a Wizard spell of 1st level or higher, you can add it to your Spellbook if it is of a Spell Level you can prepare and if you can spare the time to decipher and copy it.
+		   
+		   Copying that spell into your Spellbook involves reproducing the basic form of the spell, then deciphering the unique system of notation used by the Wizard who wrote it. You must practice the spell until you understand the sounds or gestures required, then transcribe it into your Spellbook using your own notation.
+		   
+		   For each level of the spell, the process takes 2 hours and costs 50 gp. The cost represents material Components you expend as you experiment with the spell to Master it, as well as the fine inks you need to record it. Once you have spent this time and money, you can prepare the spell just like your other Spells.
+		   
+		   Replacing the Book: You can copy a spell from your own Spellbook into another book—for example, if you want to make a backup copy of your Spellbook. This is just like copying a new spell into your Spellbook, but faster and easier, since you understand your own notation and already know how to cast the spell. You need spend only 1 hour and 10 gp for each level of the copied spell.
+		   
+		   If you lose your Spellbook, you can use the same procedure to transcribe the Spells that you have prepared into a new Spellbook. Filling out the remainder of your Spellbook requires you to find new Spells to do so, as normal. For this reason, many wizards keep backup spellbooks in a safe place.
+		   
+		   The Book’s Appearance: Your Spellbook is a unique compilation of Spells, with its own decorative flourishes and margin notes. It might be a plain, functional leather volume that you received as a gift from your Master, a finely bound gilt-­edged tome you found in an ancient Library, or even a loose collection of notes scrounged together after you lost your previous Spellbook in a mishap.'--<Description, nvarchar(max),>
            ,'Wizard_YourSpellbook'--<Code, nvarchar(50),>
 		   ),
            ('Arcane Recovery'--<Name, nvarchar(50),>
            ,'You have learned to regain some of your magical energy by studying your Spellbook. Once per day when you finish a Short Rest, you can choose expended Spell Slots to recover. The Spell Slots can have a combined level that is equal to or less than half your Wizard level (rounded up), and none of the slots can be 6th level or higher.
-
-For example, if you’re a 4th-level Wizard, you can recover up to two levels worth of Spell Slots. You can recover either a 2nd-level spell slot or two 1st-level Spell Slots.'--<Description, nvarchar(max),>
+		   
+		   For example, if you’re a 4th-level Wizard, you can recover up to two levels worth of Spell Slots. You can recover either a 2nd-level spell slot or two 1st-level Spell Slots.'--<Description, nvarchar(max),>
            ,'Wizard_ArcaneRecovery'--<Code, nvarchar(50),>
 		   ),
            ('Arcane Tradition'--<Name, nvarchar(50),>
@@ -35,14 +35,14 @@ For example, if you’re a 4th-level Wizard, you can recover up to two levels wo
 		   ),
            ('Spell Mastery'--<Name, nvarchar(50),>
            ,'At 18th level, you have achieved such mastery over certain Spells that you can cast them at will. Choose a 1st-level Wizard spell and a 2nd-level Wizard spell that are in your Spellbook. You can cast those Spells at their lowest level without expending a spell slot when you have them prepared. If you want to cast either spell at a higher level, you must expend a spell slot as normal.
-
-By spending 8 hours in study, you can exchange one or both of the Spells you chose for different Spells of the same levels.'--<Description, nvarchar(max),>
+		   
+		   By spending 8 hours in study, you can exchange one or both of the Spells you chose for different Spells of the same levels.'--<Description, nvarchar(max),>
            ,'Wizard_SpellMastery'--<Code, nvarchar(50),>
 		   ),
            ('Signature Spells'--<Name, nvarchar(50),>
            ,'When you reach 20th level, you gain mastery over two powerful Spells and can cast them with little effort. Choose two 3rd-level Wizard Spells in your Spellbook as your signature Spells. You always have these Spells prepared, they don''t count against the number of Spells you have prepared, and you can cast each of them once at 3rd level without expending a spell slot. When you do so, you can''t do so again until you finish a short or Long Rest.
-
-If you want to cast either spell at a higher level, you must expend a spell slot as normal.'--<Description, nvarchar(max),>
+		   
+		   If you want to cast either spell at a higher level, you must expend a spell slot as normal.'--<Description, nvarchar(max),>
            ,'Wizard_SignatureSpells'--<Code, nvarchar(50),>
 		   ),
 
@@ -328,12 +328,12 @@ INSERT INTO [dbo].[ClassFeature]
            ,(SELECT TOP(1) Id FROM Feature WHERE [Code] = 'PreparingSpells')--<FeatureId, int,>
 		   ,1--<Level, int>
            ,'The Wizard table shows how many Spell Slots you have to cast your Spells of 1st level and higher. To cast one of these Spells, you must expend a slot of the spell''s level or higher. You regain all expended Spell Slots when you finish a Long Rest.
-
-You prepare the list of Wizard Spells that are available for you to cast. To do so, choose a number of Wizard Spells from your Spellbook equal to your Intelligence modifier + your Wizard level (minimum of one spell). The Spells must be of a level for which you have Spell Slots.
-
-For example, if you''re a 3rd-level Wizard, you have four 1st-level and two 2nd-level Spell Slots. With an Intelligence of 16, your list of prepared Spells can include six Spells of 1st or 2nd level, in any combination, chosen from your Spellbook. If you prepare the 1st-level spell Magic Missile, you can cast it using a 1st-level or a 2nd-level slot. Casting the spell doesn''t remove it from your list of prepared Spells.
-
-You can change your list of prepared Spells when you finish a Long Rest. Preparing a new list of Wizard Spells requires time spent studying your Spellbook and memorizing the incantations and gestures you must make to cast the spell: at least 1 minute per Spell Level for each spell on your list.'--<Description, int,>
+		   
+		   You prepare the list of Wizard Spells that are available for you to cast. To do so, choose a number of Wizard Spells from your Spellbook equal to your Intelligence modifier + your Wizard level (minimum of one spell). The Spells must be of a level for which you have Spell Slots.
+		   
+		   For example, if you''re a 3rd-level Wizard, you have four 1st-level and two 2nd-level Spell Slots. With an Intelligence of 16, your list of prepared Spells can include six Spells of 1st or 2nd level, in any combination, chosen from your Spellbook. If you prepare the 1st-level spell Magic Missile, you can cast it using a 1st-level or a 2nd-level slot. Casting the spell doesn''t remove it from your list of prepared Spells.
+		   
+		   You can change your list of prepared Spells when you finish a Long Rest. Preparing a new list of Wizard Spells requires time spent studying your Spellbook and memorizing the incantations and gestures you must make to cast the spell: at least 1 minute per Spell Level for each spell on your list.'--<Description, int,>
            ,0--<Hidden, bit,>
 		   ),
            ((SELECT TOP(1) Id FROM Class WHERE [Name] = 'Wizard')--<ClassId, int,>
@@ -341,9 +341,10 @@ You can change your list of prepared Spells when you finish a Long Rest. Prepari
            ,(SELECT TOP(1) Id FROM Feature WHERE [Code] = 'SpellcastingAbility')--<FeatureId, int,>
 		   ,1--<Level, int>
            ,'Intelligence is your Spellcasting Ability for your Wizard Spells, since you learn your Spells through dedicated study and memorization. You use your Intelligence whenever a spell refers to your Spellcasting Ability. In addition, you use your Intelligence modifier when setting the saving throw DC for a Wizard spell you cast and when Making an Attack roll with one.
-
-Spell save DC = 8 + your Proficiency Bonus + your Intelligence modifier
-Spell Attack modifier = your Proficiency Bonus + your Intelligence modifier'--<Description, int,>
+		   
+		   Spell save DC = 8 + your Proficiency Bonus + your Intelligence modifier
+		   
+		   Spell Attack modifier = your Proficiency Bonus + your Intelligence modifier'--<Description, int,>
            ,1--<Hidden, bit,>
 		   ),
            ((SELECT TOP(1) Id FROM Class WHERE [Name] = 'Wizard')--<ClassId, int,>
